80ce29c48f250be03882416fcad4f25f1ac1777d
Technical-See <amitaec.786@gmail.com>
2020-11-25 10:16:50 +0530

    Add Meta Tags in Header from function file

---
+++ added_code_in_Header.php
@@ -0,0 +1,15 @@
+//Function to add Meta Tags in Header without Plugin
+function add_meta_tags() {
+?>
+  <meta name="meta_name" content="meta_value"/>
+<?php }
+add_action('wp_head', 'add_meta_tags');
+
+//Function to add Meta Tags in Header without Plugin
+
+//Function to add Meta Tags in Header without Plugin
+function add_meta_tags() {
+  echo '<meta name="meta_name" content="meta_value" />';
+}
+add_action('wp_head', 'add_meta_tags');
+//Function to add Meta Tags in Header without Plugin
