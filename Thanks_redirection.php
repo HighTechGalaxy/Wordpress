f26ac823a8520ede5b110dee144934f77066e1e4
Technical-See <amitaec.786@gmail.com>
2021-03-11 18:35:43 +0530

    Update Thanks_redirection.php

--- Thanks_redirection.php
+++ Thanks_redirection.php
@@ -3,3 +3,12 @@ document.addEventListener( 'wpcf7mailsent', function( event ) {
   location = 'http://example.com/';
 }, false );
 </script>
+
+ or
+
+
+<script>
+document.addEventListener( 'wpcf7mailsent', function( event ) {
+    location = "<?php echo get_bloginfo('url'); ?>/thank-you";
+}, false );
+</script>
