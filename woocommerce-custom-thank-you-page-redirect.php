b01cd28eea9d25016f64bea7bff88dcbe5ab3000
Technical-See <amitaec.786@gmail.com>
2021-03-11 18:53:53 +0530

    Update woocommerce-custom-thank-you-page-redirect.php

--- woocommerce-custom-thank-you-page-redirect.php
+++ woocommerce-custom-thank-you-page-redirect.php
@@ -11,3 +11,48 @@ function woo_custom_redirect_after_purchase() {
 	}
 }
 ?>
+
+
+
+<?php
+// Changing the Thank You page title & placed in the file functions.php
+
+add_filter( 'the_title', 'woo_title_order_received', 10, 2 );
+
+function woo_title_order_received( $title, $id ) {
+	if ( function_exists( 'is_order_received_page' ) && 
+	     is_order_received_page() && get_the_ID() === $id ) {
+		$title = "Thank you for your order! :)";
+	}
+	return $title;
+}
+
+
+
+<?php
+	
+	// You can go one step forward & also personalize the Thank You page title to include details like the customer’s name, or anything else.You can add the below code in a plugin or in your theme’s functions.php:
+
+add_filter( 'the_title', 'woo_personalize_order_received_title', 10, 2 );
+function woo_personalize_order_received_title( $title, $id ) {
+    if ( is_order_received_page() && get_the_ID() === $id ) {
+	    global $wp;
+
+        // Get the order. Line 9 to 17 are present in order_received() in includes/shortcodes/class-wc-shortcode-checkout.php file
+        $order_id  = apply_filters( 'woocommerce_thankyou_order_id', absint( $wp->query_vars['order-received'] ) );
+        $order_key = apply_filters( 'woocommerce_thankyou_order_key', empty( $_GET['key'] ) ? '' : wc_clean( $_GET['key'] ) );
+
+        if ( $order_id > 0 ) {
+            $order = wc_get_order( $order_id );
+            if ( $order->get_order_key() != $order_key ) {
+                $order = false;
+            }
+        }
+
+        if ( isset ( $order ) ) {
+            //$title = sprintf( "You are awesome, %s!", esc_html( $order->billing_first_name ) ); // use this for WooCommerce versions older then v2.7
+	    $title = sprintf( "You are awesome, %s!", esc_html( $order->get_billing_first_name() ) );
+        }
+    }
+    return $title;
+}
