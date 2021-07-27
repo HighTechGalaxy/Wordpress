7c33342de0555b4320522fef0dcf86fe7d12c3f7
Technical-Galaxy <amitaec.786@gmail.com>
2021-07-27 15:30:21 +0530

    Update Thanks_redirection.php

--- Thanks_redirection.php
+++ Thanks_redirection.php
@@ -15,31 +15,24 @@ document.addEventListener( 'wpcf7mailsent', function( event ) {
 
 
 
-#Here is an example of the code for two different pages.
+#Here is an example of the code for two different pages USING FOR ID.
 
 
+add_action( 'wp_footer', 'redirect_cf7' );
+function redirect_cf7() {
+?>
+<script type="text/javascript">
+document.addEventListener( 'wpcf7mailsent', function( event ) {
+   if ( '101' == event.detail.contactFormId ) { //if the form if equals #101
+      location = 'https://www.example.com/thanks1/';
+    } else if ( '365' == event.detail.contactFormId ) { //if the form if equals #365
+      location = 'https://www.example.com/thanks2/';
+    } else if ( '987' == event.detail.contactFormId ) { //if the form if equals #987
+      location = 'https://www.example.com/thanks3/';
+    } else { // Default thank you page for all forms which are not 101, 365 or 987
+      location = 'https://www.example.com/thanks-main/';
+    }
+}, false );
+</script>
 <?php
- 
-//This function prints the JavaScript to the footer
-function cf7_footer_script(){ 
- 
-//if page name is contact.
-if ( is_page('contact')) {?>
-  
-    <script>
-    document.addEventListener( 'wpcf7mailsent', function( event ) {
-            location = 'http://example.com/thank-you';
-        }, false );
-    </script>
- 
-<?php } else if ( is_page('download')) /* if page name is download */ {?>
-  
-    <script>
-    document.addEventListener( 'wpcf7mailsent', function( event ) {
-            location = 'http://example.com/thank-you';
-        }, false );
-    </script>
-  
-<?php } 
-     
 }
